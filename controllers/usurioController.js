5830de6157d4d1138952cc4aea896d96170b6c38
juanvacag <>
2022-07-23 07:47:09 +0200

    CORS

--- controllers/usurioController.js
+++ controllers/usurioController.js
@@ -7,15 +7,15 @@ const registrar = async (req, res) => {
     const {email} = req.body
     const existeUsuario = await Usuario.findOne({email})
     if(existeUsuario) {
-        const error = new error("Usuario ya registrado")
+        const error = new Error("Usuario ya registrado")
         return res.status(400).json({msg: error.message})
     }
 
     try {
         const usuario = new Usuario(req.body)
         usuario.token = generarId()
-        const usuarioAlmacenado = await usuario.save()
-        res.json(usuarioAlmacenado)
+        await usuario.save()
+        res.json({msg: 'Usuario Creado Correctamente, revisa tu Email para comfirmar tu cuenta'})
 
     } catch (error) {
         console.log(error)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import conectarDB from './config/db.js';
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import proyectoRoutes from './routes/proyectoRoutes.js'
@@ -11,6 +12,23 @@ app.use(express.json())
 dotenv.config()
 
 conectarDB()
+
+//configurar CORS
+const whitelist = ['http://127.0.0.1:5173']
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        //console.log(origin)
+        if(whitelist.includes(origin)) {
+            //puede consultar la API
+            callback(null, true)
+        }else {
+            //no esta permitido
+            callback(new Error("Error de Cors"))
+        }
+    }
+}
+app.use(cors(corsOptions))
 
 //routings
 app.use("/api/usuarios", usuarioRoutes)
